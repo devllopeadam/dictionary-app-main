7c02cb7966fef25b6b4b59ada5a5fc7dfa2f9d5e
devllopeadam <jeniahadam@gmail.com>
2024-04-12 22:32:56 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-xl text-sky-400">
+      jeniah adam is one of the best software enginer in the world and thank you
+    </h1>
+  );
+}
+
+export default App;
